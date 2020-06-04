705907a7695479ec1f7708476994e2765a48527b
Aaron Huggins <ahuggins@nethealthshops.com>
2020-06-04 09:19:56 -0500

    Add test for parsing streams to get 90% coverage

--- test/AS2ParserSuite.ts
+++ test/AS2ParserSuite.ts
@@ -1,6 +1,7 @@
 import 'mocha'
 import { AS2MimeNode, AS2Parser } from '../core'
 import { ENCRYPTED_CONTENT } from './Helpers'
+import { Duplex } from 'stream'
 
 describe('AS2Parser', async () => {
   it('should parse mime message to AS2MimeNode and match parsed contents', async () => {
@@ -13,8 +14,25 @@ describe('AS2Parser', async () => {
       )
     }
 
-    // const rebuilt = await result.build()
-    // const parsed = rebuilt.toString('utf8')
+    if (ENCRYPTED_CONTENT !== result.raw) {
+      throw new Error(
+        `Mime section not correctly parsed.\nExpected: '${ENCRYPTED_CONTENT}'\nReceived: '${result.raw}'`
+      )
+    }
+  })
+
+  it('should parse stream to AS2MimeNode', async () => {
+    const stream = new Duplex()
+    stream.push(ENCRYPTED_CONTENT)
+    stream.push(null)
+    const result = await AS2Parser.parse(stream)
+    if (!(result instanceof AS2MimeNode)) {
+      throw new Error(
+        `Result was not an AS2MimeNode.\nExpected: 'AS2MimeNode'\nReceived: '${
+          (result as any).constructor.name
+        }'`
+      )
+    }
 
     if (ENCRYPTED_CONTENT !== result.raw) {
       throw new Error(
